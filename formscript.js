e8dbbd0812bdd8d51714d4ca1b978eb0bcb11ebe
Pradhan2017 <33887814+Pradhan2017@users.noreply.github.com>
2018-01-08 14:01:38 +0530

    update

--- formscript.js
+++ formscript.js
@@ -1,7 +1,7 @@
 function fetchCities(str)
 {
   var req=new XMLHttpRequest();
-  req.open("get","https://pradhan2017.github.io/Simple-Ajax-Example/cities.php?state="+str,true);
+  req.open("get","http://cities.php?state="+str,true);
   req.send();
   req.onreadystatechange=function(){
     if(req.readyState==4&&req.status==200)
